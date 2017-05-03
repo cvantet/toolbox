4cbd6ad79b9420950ee3de1856bff77d74e9931c
Christophe Vantet <christophe.vantet@gmail.com>
2017-05-03 21:53:28 +0800

    adding merging script
    Small script to merge a list of pdfs into a single one

---
+++ merge_pdfs.py
@@ -0,0 +1,27 @@
+import PyPDF2, os
+
+def merge_pdf(src_path='.', output_file='combinedminutes.pdf'):
+    pdfFiles = []
+    for filename in os.listdir(src_path):
+        if filename.endswith('.pdf'): pdfFiles.append(filename)
+    pdfFiles.sort(key=str.lower)
+    pdfWriter = PyPDF2.PdfFileWriter()   
+    pdfOutputFile = open('combinedminutes.pdf', 'wb')
+    for pdf in pdfFiles:
+        pdfFile = open(pdf,'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFile)
+        for pageNum in range(pdfReader.numPages):
+            pageObj = pdfReader.getPage(pageNum)
+            pdfWriter.addPage(pageObj)
+        pdfWrite.write(pdfOutputFile)
+        pdfFile.close()
+    pdfOutputFile.close()
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Merge PDF')
+parser.add_argument('src_path', type=str, help='Source Directory')
+parser.add_argument('-o', dest='output', type=str, help='Target file')
+
+args = parser.parse_args()
+merge_pdf(args.src_path, args.output)
