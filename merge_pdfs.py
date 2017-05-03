04f7bf4e6f66aa9690829e4e391bb149d3eca45c
Christophe Vantet <christophe.vantet@gmail.com>
2017-05-03 23:44:45 +0800

    Correcting Merge_Pdfs

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -6,13 +6,14 @@ def merge_pdf(src_path='.', output_file='combinedminutes.pdf'):
         if filename.endswith('.pdf'): pdfFiles.append(os.path.join(src_path, filename))
     pdfFiles.sort(key=str.lower)
     pdfWriter = PyPDF2.PdfFileWriter()   
-    pdfOutputFile = open('combinedminutes.pdf', 'wb')
+    pdfOutputFile = open(output_file, 'wb')
     for pdf in pdfFiles:
         pdfFile = open(pdf,'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFile)
         for pageNum in range(pdfReader.numPages):
             pageObj = pdfReader.getPage(pageNum)
             pdfWriter.addPage(pageObj)
+        print '{} copied from {}'.format(pdfReader.numPages, pdf)
         pdfWriter.write(pdfOutputFile)
         pdfFile.close()
     pdfOutputFile.close()
@@ -21,7 +22,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Merge PDF')
 parser.add_argument('src_path', type=str, help='Source Directory')
-parser.add_argument('-o', dest='output', type=str, help='Target file')
+parser.add_argument('-o', dest='output', type=str, help='Target file', default='combinedminutes.pdf')
 
 args = parser.parse_args()
 merge_pdf(args.src_path, args.output)
